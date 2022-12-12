31a5f7308012f78014ccc8f35d6d44974c338055
SENG Sourng <sengsourng@istc.edu.kh>
2022-12-12 17:52:34 +0700

    upload static

--- 01_seng_sourng/index.php
+++ 01_seng_sourng/index.php
@@ -97,5 +97,22 @@
     myTestGlobal();
     echo $y; // outputs 15
 ?>
+
+
+<?php
+    $x = 5;
+    $y = 10;
+
+    function myTestGobal2() {
+    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+    }
+
+    myTestGobal2();
+    echo $y; // outputs 15
+?>
+
+
+
+
 </body>
 </html>
