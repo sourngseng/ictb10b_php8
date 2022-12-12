112fbc000c61fb9f848a4669f000094d3c638afc
DIT-NIT <bronitbronit14@gmail.com>
2022-12-12 17:36:37 +0700

    UPdate

--- 19_loeut_nit/index.php
+++ 19_loeut_nit/index.php
@@ -1,8 +1,8 @@
 <?php 
 
 echo "<h1>hello nit</h1>";
-define("data_name","01_loeut_nit");
+define("data_name","loeut_nit");
+define("data_jop","I am a student");
 echo "my name is ".data_name;
-
-
+echo "<br>".data_jop;
 ?>
