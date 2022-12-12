d38667f546fff3648ad5c48147a6f3c98f007437
sreyvoukVorng <119840256+sreyvoukVorng@users.noreply.github.com>
2022-12-12 21:23:33 +0700

    user

---
+++ 20_sreyvouk/index.php
@@ -0,0 +1,13 @@
+<?php
+    // Declaring variables : ប្រកាសអថេរ
+    $txt = "Hello World!";
+    $number = 10;
+    // Displaying variables value បង្ហាញតម្លៃអថេរ
+    echo $txt; // Output: Hello World!
+    echo "<br>";
+    echo $number; // Output: 10
+    
+    $number = 23;
+    echo "<br>";
+    echo $number; // Output: 10    
+?>
