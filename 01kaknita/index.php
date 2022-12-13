eb6bb58ac44bd472f32a367f4addb4589a9f4d9a
kaknita <119846067+kaknita@users.noreply.github.com>
2022-12-12 22:06:56 -0800

    upload

--- 01kaknita/index.php
+++ 01kaknita/index.php
@@ -1,3 +1,10 @@
 <?php
-echo "hello";
+    $text = "Hello World!";
+    $number = 90;
+    echo $text; 
+    echo "<br>";
+    echo $number;
+    $number = 70;
+    echo "<br>";
+    echo $number;     
 ?>
