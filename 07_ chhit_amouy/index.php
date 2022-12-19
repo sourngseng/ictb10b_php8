0f2ccc7ca2c26a2b298adecb1ac3088e1f58254f
chhitamouy <chhitamouy6@gmail.com>
2022-12-19 15:52:14 +0700

    using variable

--- 07_ chhit_amouy/index.php
+++ 07_ chhit_amouy/index.php
@@ -1,6 +1,31 @@
 <?php
-$txt = "<h1>Hello world !</h1>";
-$number = 10;
-echo $txt;
-echo "<h1>$number</h1"; 
+    // Declaring variables : ប្រកាសអថេរ
+    $txt = "Hello World!";
+    $number = 10;
+    // Displaying variables value បង្ហាញតម្លៃអថេរ
+    echo $txt; // Output: Hello World!
+    echo "<br>";
+    echo $number; // Output: 10
+
+    $number = 23;
+    echo "<br>";
+    echo $number; // Output: 10    
+?>
+
+<?php
+    // Defining constant
+    define("SITE_URL", "https://www.tutorialrepublic.com/");
+    // Using constant
+    echo '<br>Thank you for visiting - ' . SITE_URL;
+
+    define("databse_name","sourng_db");
+    define("user_name","root");
+    define("password","Pwd123");
+
+
+    echo "<br>My Databse name is :". databse_name;
+    // define("databse_name","my_db");
+
+    echo "<br>My Databse name is :". databse_name;
+
 ?>
