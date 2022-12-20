496191ddcd6832ac1ea7ae530d8bef96eba6b789
NgekSak <ngeksak0707@gmail.com>
2022-12-20 01:06:32 -0800

    variable_card

---
+++ 05_ngek_sak/04_ex02_Variable_card.php
@@ -0,0 +1,49 @@
+<?php 
+    $personInfo=array("Sou yean","seyha","Sak","Nit","Heat");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <h2>Card Image</h2>
+  <p></p>
+  <div class="card" style="width:400px">
+  <img class="card-img-bottom" src="./img/1.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Sou yean</h4>
+      <p class="card-text">ICT Sutdent</p>
+      <p>Phone</p>
+      <p>Email</p>
+      <p>Adress</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+  <br>
+  
+  <p>Information:</p>
+  <div class="card" style="width:400px">
+  <img class="card-img-bottom" src="./img/2.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">seyha</h4>
+     <p class="card-text">ICT stu2</p>
+      <p>Phone</p>
+      <p>Email</p>
+      <p>Adress</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+        </div>
+        
+      </div>
+    </div>
+
+    </body>
+    </html
