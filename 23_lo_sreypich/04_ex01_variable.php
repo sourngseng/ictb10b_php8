a1d751416aaa7c92d8bb7689c213e47df8f397b8
DIT-NIT <bronitbronit14@gmail.com>
2022-12-22 17:43:42 +0700

    variablecard

---
+++ 23_lo_sreypich/04_ex01_variable.php
@@ -0,0 +1,9 @@
+<?php
+    $text="<h1>hello Khoude</h1>";
+    $number=19;
+    echo $text;
+    echo "<h1>$number</h1>";
+    define("your_name","<h1>lo_sreypich</h1>");
+    echo "<h1>my name is</h1>".your_name;
+
+?>
