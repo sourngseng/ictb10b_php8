81d5e0fd43e35df3a3c0faed10a56451bb698297
DIT-NIT <bronitbronit14@gmail.com>
2022-12-26 16:38:22 +0700

    form_get

--- 19_loeut_nit/05_ex03_variable_ds.php
+++ 19_loeut_nit/05_ex03_variable_ds.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body{
-            text-align:center;
-        }
-    </style>
 </head>
 <body>
     <?php
@@ -23,4 +18,4 @@
         echo "<img src='$photo'";
     ?>
 </body>
-</html>
+</html>f
