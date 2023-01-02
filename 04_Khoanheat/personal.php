41c52393385232dbadcabfe686f0d6f5a3b8d080
Khoan Heat <heatkhoan@gmail.com>
2023-01-02 16:30:42 +0700

    personal

---
+++ 04_Khoanheat/personal.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My First Local PHP File</title>
+	</head>
+	<body>
+		<h1>My First Local PHP File</h1>
+		<?php
+		echo '<p>PHP is working<p>';
+		?>
+	</body>
+</html>
