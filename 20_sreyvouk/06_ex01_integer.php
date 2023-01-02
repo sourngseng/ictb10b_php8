139c0017ea63caac5be6e78da33b49d4f189afe3
sreyvoukVorng <119840256+sreyvoukVorng@users.noreply.github.com>
2023-01-02 16:25:33 +0700

    PHP_integer

---
+++ 20_sreyvouk/06_ex01_integer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>integer</title>
+</head>
+<body>
+ <?php 
+ $a =123;
+ var_dump($a);
+ echo "<br";
+ $b =-567;
+ var_dump($b);
+ echo "<br>";
+ $c =0x45B;
+ var_dump($c);
+ echo "<br>";
+ $d =023;
+ var_dump($d);
+ echo "<br>";
+ ?>   
+</body>
+</html>
