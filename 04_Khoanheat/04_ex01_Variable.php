c12f6f892633fe2d48db14e38f2a863aa1a808da
Khoan Heat <heatkhoan@gmail.com>
2023-01-08 10:57:42 +0700

    Bootstrap

--- 04_Khoanheat/04_ex01_Variable.php
+++ 04_Khoanheat/04_ex01_Variable.php
@@ -1,46 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-  <head>
-     <title>Bootstrap Example</title>
-        <meta charset="utf-8">
-           <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-     <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">KhoanHeat</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-    <span class="navbar-toggler-icon"></span>
-=======
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<title>Bootstrap Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">Hello</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-      <span class="navbar-toggler-icon"></span>
->>>>>>> parent of 9a61c0a (04_ex01_Variable)
-    </button>
-    <div class="collapse navbar-collapse" id="mynavbar">
-    <ul class="navbar-nav me-auto">a
+<div class="container-fluid">
+<a class="navbar-brand" href="javascript:void(0)">KhoanHeat</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="navbar-nav me-auto">
 <li class="nav-item">
-       <a class="nav-link" href="javascript:void(0)">Home</a>
-    </li>
-    <li class="nav-item">
-       <a class="nav-link" href="javascript:void(0)">About Us</a>
-    </li>
-<li class="nav-item">
-<a class="nav-link" href="javascript:void(0)">Contact Us</a>
+<a class="nav-link" href="javascript:void(0)">Home</a>
 </li>
 </ul>
 <form class="d-flex">
@@ -52,6 +30,8 @@
 </nav>
 
 <?php
+    
+    @$_name=$_POST['name'];
     @$_email=$_POST['email'];
     @$_pswd=$_POST['pswd'];
     @$_remember=$_POST['remember'];
@@ -62,14 +42,13 @@
 <div class="row">
 <div class="col-md-6 bg-info card pb-5">
 <h2>បញ្ចូលពត៌មាន</h2>
-<form action="04_ex01_Variable.php" method="POST">
+<form action="05_ex01_Variable .php" method="POST">
 
 <div class="mb-3 mt-3">
 <label for=name">Name:</label>
 <input type="name" class="form-control" id="name" placeholder="Enter name" name="name">
 </div>
 
-<<<<<<< HEAD
 <div class="mb-3 mt-3">
 <label for="email">Email:</label>
 <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
@@ -102,7 +81,3 @@
 
 </body>
 </html>
-=======
-</body>
-</html>
->>>>>>> parent of 9a61c0a (04_ex01_Variable)
