ffb83316c5f7284a47f0d9e9f317701736ee80a5
MoungSouYean <120002889+MoungSouYean@users.noreply.github.com>
2023-01-09 15:54:06 +0700

    incrment_deincrement

---
+++ 16_mong_souyean/Lesson_07/07_ex04_increment_deincrement.php
@@ -0,0 +1,25 @@
+<?php
+
+$x =10;
+echo ++$x;
+echo $x;
+echo"<br>";
+
+$x =10;
+echo $x++;
+echo $x;
+echo"<br>";
+
+$x =10;
+echo --$x;
+echo $x;
+echo"<br>";
+
+$x =10;
+echo $x--;
+echo $x;
+echo"<br>";
+
+
+
+?>
