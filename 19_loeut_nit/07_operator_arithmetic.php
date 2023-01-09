bc4f90fde5562af19f6f015505bd34c71061375f
DIT-NIT <bronitbronit14@gmail.com>
2023-01-09 15:32:28 +0700

    arithmetic in php

---
+++ 19_loeut_nit/07_operator_arithmetic.php
@@ -0,0 +1,14 @@
+<?php
+$a=20;
+$b=30;
+$c="20"; 
+var_dump ($a==$c);  //result true
+var_dump ($a===$c); //result false
+var_dump ($a!=$b);  //result true
+var_dump ($a!==$c); //result true
+var_dump ($a<$b);   //result true
+var_dump ($a>$b);   //result false
+var_dump ($a<=$b);  //result true
+var_dump ($a>=$b);  //result false
+
+?>
