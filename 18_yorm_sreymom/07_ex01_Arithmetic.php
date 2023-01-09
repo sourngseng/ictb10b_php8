4e698e270fd779633dd2fbd09d20dfed32f6cb62
yormsreymom <yormsreymom@gmail.com>
2023-01-09 16:09:48 +0700

    find_Replace

---
+++ 18_yorm_sreymom/07_ex01_Arithmetic.php
@@ -0,0 +1,18 @@
+<?php
+$x= 10;
+$x= 4;
+echo("Result of $x+$y=" . $x+$y);//outputs:14
+echo "<br>";
+echo("Result of $x-$y=" . $x-$y);//outputs:6
+echo "<br>";
+echo("Result of $x * $y=" .$x*$y);//outputs:40
+echo "<br>";
+echo("Result of $x / $y=" .$x/$y);//outputs:2.5
+echo "<br>";
+echo( "Result of $x % $y=" . $x%$y);//outputs:2
+echo "<br>";
+
+$result=2*($x+$y)/3;
+echo"<br>";
+echo("កន្សោមប្រមាណវិធី =" . $result);
+?>
