70f2e6b73a0d7665b5bfd8d90b3aad98dd3d1576
yormsreymom <yormsreymom@gmail.com>
2023-01-09 16:27:39 +0700

    Arithmetic

--- 18_yorm_sreymom/07_ex01_Arithmetic.php
+++ 18_yorm_sreymom/07_ex01_Arithmetic.php
@@ -1,18 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$x= 10;
-$x= 4;
-echo("Result of $x+$y=" . $x+$y);//outputs:14
-echo "<br>";
-echo("Result of $x-$y=" . $x-$y);//outputs:6
-echo "<br>";
-echo("Result of $x * $y=" .$x*$y);//outputs:40
-echo "<br>";
-echo("Result of $x / $y=" .$x/$y);//outputs:2.5
-echo "<br>";
-echo( "Result of $x % $y=" . $x%$y);//outputs:2
-echo "<br>";
+    $x=10;
+    $y=4;
+    echo "Result of $x+$y=" .($x+$y);//outputs:14
+    echo "<br>";
+    echo"Result of $x-$y=" .($x-$y);//outputs:6
+    echo "<br>";
+    echo"Result of $x * $y=" .($x*$y);//outputs:40
+    echo "<br>";
+    echo"Result of $x / $y=" .($x/$y);//outputs:2.5
+    echo "<br>";
+    echo"Result of $x % $y=" .($x%$y);//outputs:2
+    echo "<br>";
 
-$result=2*($x+$y)/3;
-echo"<br>";
-echo("កន្សោមប្រមាណវិធី =" . $result);
+    $result=2*($x+$y)/3;
+    echo"<br>";
+    echo("កន្សោមប្រមាណវិធី =" .$result);
 ?>
+</body>
+</html>
