82dae5728f3b7b6c54a59fa819110567ce05db19
SamnangOrg <119838746+SamnangOrg@users.noreply.github.com>
2023-01-12 06:40:30 +0700

    Comparison Operators

--- 11_phon_samnang/07_ex05_Logical.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $year = 2014;
-    if(($year % 400 ==0) || (($year % 100 != 0) && ($year % 4 ==0))){
-        echo "$year is a lear year.";
-    }else{
-        echo"$year is nor a leap year.";
-    }
-
-?>
