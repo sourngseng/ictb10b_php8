3e66fc4495e6fde4337b97ce98ce28c13e346739
SamnangOrg <119838746+SamnangOrg@users.noreply.github.com>
2023-01-12 06:23:31 +0700

    Assignment Operators

---
+++ 11_phon_samnang/07_ex07_Arrary.php
@@ -0,0 +1,13 @@
+<?php
+    $x = array("a" => "Rad", "b" => "Green", "c" => "Blue");
+    $y = array("u" => "Yellow", "v" => "Oranfe", "w" => "Pink");
+    $z = $x + $y;
+    var_dump($z);
+    var_dump($x == $y);
+    var_dump($x === $y);
+    var_dump($x != $y);
+    var_dump($x <> $y);
+    var_dump($x !== $y);
+
+
+?>
